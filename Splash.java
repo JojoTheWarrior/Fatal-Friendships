829860153571b185e6269b384e3b2b8ef20b7f82
a huang <124748265+dogwter@users.noreply.github.com>
2023-05-25 09:32:06 -0400

    Add files via upload

--- Splash.java
+++ Splash.java
@@ -5,63 +5,164 @@ import javax.swing.*;
 import javax.imageio.*;
 import java.io.*;
 
-public class Splash implements MouseMotionListener{
+public class Splash implements MouseMotionListener, MouseListener, KeyListener {
     Drawing draw = new Drawing();
     int x, y, z = 0;
+    int state = 0;
+    int choice = 0;
 
     public Splash() {
         JFrame frame = new JFrame("Fatal Friendships");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 520);
         draw.addMouseMotionListener(this);
+        draw.addMouseListener(this);
+        draw.addKeyListener(this);
+        draw.setFocusable(true);
         frame.add(draw);
         frame.setVisible(true);
     }
 
-    public void mouseMoved(MouseEvent e){
-        x = e.getX();
-        y = e.getY();
-        if(x >= 300 && x <= 500 && y <= 415 && y >= 365) z = 1;
-        else z = 0;
-        
-        draw.repaint();
+    public void mouseMoved(MouseEvent e) {
+        if (state == 0) {
+            x = e.getX();
+            y = e.getY();
+            if (x >= 300 && x <= 500 && y <= 415 && y >= 365)
+                z = 1;
+            else
+                z = 0;
+
+            draw.repaint();
+        }
     }
 
-    public void mouseDragged(MouseEvent e){
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        if (state == 0) {
+            x = e.getX();
+            y = e.getY();
+            if (x >= 300 && x <= 500 && y <= 415 && y >= 365) {
+                state = 1;
+            }
+            draw.repaint();
+        } else if (state == 1) {
+            x = e.getX();
+            y = e.getY();
+            if (x >= 300 && x <= 500 && y <= 415 && y >= 365) {
+                state = 2;
+            }
+            draw.repaint();
+        }
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void keyTyped(KeyEvent e) {
+        if(state == 1){
+            if(choice < 2 && e.getKeyChar() == 's') choice ++;
+            else if(choice > 0 && e.getKeyChar() == 'w') choice --;
+            draw.repaint();
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
     }
 
     class Drawing extends JComponent {
 
-        public void splashScreen(Graphics g){
+        public void splashScreen(Graphics g) {
             Font customFont = null;
             try {
-                customFont = Font.createFont(Font.TRUETYPE_FONT, new File("DeterminationMonoWebRegular-Z5oq.ttf")).deriveFont(90f);
+                customFont = Font
+                        .createFont(Font.TRUETYPE_FONT,
+                                new File(
+                                        "C:\\Users\\342537420\\Downloads\\Hello\\DeterminationMonoWebRegular-Z5oq.ttf"))
+                        .deriveFont(90f);
                 GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
                 ge.registerFont(customFont);
             } catch (Exception e) {
-            } 
+            }
 
             BufferedImage bg;
-            try{
-                bg = ImageIO.read(new File("splash.png"));
+            try {
+                bg = ImageIO.read(new File("C:\\Users\\342537420\\Downloads\\Hello\\splash.png"));
                 g.drawImage(bg, 0, 0, 800, 500, null);
-            } catch (Exception e){
+            } catch (Exception e) {
             }
-            g.setColor(new Color(140,70,70));
+            g.setColor(new Color(140, 70, 70));
             g.setFont(customFont);
             g.drawString("friendships", 150, 170);
             g.setFont(customFont.deriveFont(50f));
-            if(z != 0) g.setColor(Color.BLACK);
-            else g.setColor(new Color(140,70,70));
+            if (z != 0)
+                g.setColor(Color.BLACK);
+            else
+                g.setColor(new Color(140, 70, 70));
             g.drawString("new game", 300, 405);
+        }
+
+        public void menu(Graphics g) {
+            Font customFont = null;
+            try {
+                customFont = Font
+                        .createFont(Font.TRUETYPE_FONT,
+                                new File(
+                                        "C:\\Users\\342537420\\Downloads\\Hello\\DeterminationMonoWebRegular-Z5oq.ttf"))
+                        .deriveFont(90f);
+                GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+                ge.registerFont(customFont);
+            } catch (Exception e) {
+            }
+
+            BufferedImage bg;
+            try {
+                bg = ImageIO.read(new File("C:\\Users\\342537420\\Downloads\\Hello\\menu.png"));
+                g.drawImage(bg, 0, 0, 800, 500, null);
+            } catch (Exception e) {
+            }
+            g.setColor(new Color(140, 70, 70));
+            g.setFont(customFont.deriveFont(40f));
+            g.drawString("play", 360, 300);
+            g.drawString("credits", 360, 350);
+            g.drawString("exit", 360, 400);
+
+            int y = 300 + choice*50;
+            g.drawString("*", 320, y);
+        }
+
+        public void levels(Graphics g) {
+            g.setColor(Color.BLACK);
+            g.drawString("real game", 150, 170);
         }
 
         public void paint(Graphics g) {
             // THIS WILL IMPLEMENT DIFF METHODS BASED ON GAMESTATE
-            splashScreen(g);
-        
+            if (state == 0) {
+                splashScreen(g);
+            } else if (state == 1) {
+                menu(g);
+            } else if (state == 2) {
+                levels(g);
+            }
+        }
     }
-}
+
     public static void main(String[] args) {
         new Splash();
     }
